4d74683f5250ff489c81ea34fd3092d6122db2b5
Muzaffer Cıkay <muzaffercky@gmail.com>
2026-02-20 21:37:57 +0300

    Add annotation to ContentExtractorProtocol

--- extractor/protocol.py
+++ extractor/protocol.py
@@ -2,5 +2,5 @@ from typing import Protocol, Any
 
 
 class ContentExtractorProtocol(Protocol):
-    def extract(self, html, url) -> Any:
+    def extract(self, html: str, url: str) -> Any:
         pass
